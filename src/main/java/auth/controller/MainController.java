783c3445b391a328951a728e85460c09efa4c12b
rainn66 <jaeseong7975@gmail.com>
2024-02-23 22:50:57 +0900

    2024.2.23 메뉴 상세

--- src/main/java/auth/controller/MainController.java
+++ src/main/java/auth/controller/MainController.java
@@ -10,6 +10,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
 
+/**
+ * request url 명명규칙
+ * 1. view, list 조회는 화면이 수행하는 총체적인 기능 중심으로
+ * 2. save, update, delete 해당 기능 중심으로 작성
+ */
 @Slf4j
 @Controller
 @RequiredArgsConstructor
--- src/main/java/auth/controller/MenuController.java
+++ src/main/java/auth/controller/MenuController.java
@@ -1,17 +1,55 @@
 package auth.controller;
 
+import auth.entity.Authority;
+import auth.entity.Menu;
+import auth.repository.AuthorityRepository;
+import auth.repository.MenuRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 @Slf4j
 @Controller
+@RequiredArgsConstructor
+@RequestMapping("/menu")
 public class MenuController {
 
-    @GetMapping("/menu/menuMng")
+    private final MenuRepository menuRepository;
+
+    private final AuthorityRepository authorityRepository;
+
+    @GetMapping("/menuMng")
     public String menuMng(Model model) {
+        List<Menu> menus = menuRepository.findByParentIsNull();
+
+        List<Authority> authorities = authorityRepository.findAll();
+
+        model.addAttribute("menus", menus);
+        model.addAttribute("authorities", authorities);
         return "menu/menu_mng";
+    }
+
+    @ResponseBody
+    @PostMapping("/menuInfo")
+    public Map<String, Object> menuInfo(@RequestBody Map<String, Object> param) {
+        log.info("param : {}", param);
+        Map<String, Object> rtnMap = new HashMap<String, Object>();
+        Optional<Menu> menuInfo = menuRepository.findById(Long.parseLong(String.valueOf(param.get("menuIdx"))));
+
+        rtnMap.put("menuInfo", menuInfo);
+
+        return rtnMap;
     }
 
 }
