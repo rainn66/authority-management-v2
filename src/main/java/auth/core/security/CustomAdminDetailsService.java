789425d39999df22f9f8966487f8e17f8e00e7b3
rainn66 <jaeseong7975@gmail.com>
2024-04-07 00:50:56 +0900

    2024.4.7 최근접속일시 처리

--- src/main/java/auth/core/security/CustomAdminDetailsService.java
+++ src/main/java/auth/core/security/CustomAdminDetailsService.java
@@ -8,6 +8,9 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
 
 @Slf4j
 @Service
@@ -17,9 +20,15 @@ public class CustomAdminDetailsService implements UserDetailsService {
     private final AdminRepository adminRepository;
 
     @Override
+    @Transactional
     public UserDetails loadUserByUsername(String userId) {
         log.info("userId : {}", userId);
-        Admin admin = adminRepository.findByUserId(userId).orElseThrow(
+        Admin admin = adminRepository.findByUserId(userId)
+                .map(m -> {
+                    m.update(LocalDateTime.now());
+                    return m;
+                })
+                .orElseThrow(
                 () -> new UsernameNotFoundException("회원 정보를 찾을 수 없습니다.")
         );
         return new CustomAdminDetails(admin);
